e8f3280d3ee6c1ce3d10113e2429865c2ca6a12c
Divyanshu Verma <divyanshuverma.atwork@gmail.com>
2026-07-24 18:47:11 +0530

    Create email_templates.py

---
+++ email_templates.py
@@ -0,0 +1,58 @@
+def first_email(first_name, title, company):
+
+    return f"""
+Hi {first_name},
+
+Hope you're doing well.
+
+I wanted to check if {company} is currently hiring Data Analyst interns.
+
+I'm pursuing the IIT Madras BS in Data Science and have been building projects using SQL, Python, Power BI, and Excel. I've attached my resume for reference.
+
+If there are any suitable openings, I'd love to be considered.
+
+Thank you for your time.
+
+Best,
+Divyanshu Verma
+LinkedIn: https://www.linkedin.com/in/divyanshuverma-career
+"""
+
+
+
+
+
+def followup1(first_name):
+
+    return f"""
+Hi {first_name},
+
+Just wanted to follow up on my previous email regarding a Data Analyst internship.
+
+I know you're busy, so I just wanted to check if there are any internship opportunities on your team, either now or in the near future.
+
+I've attached my resume again for convenience.
+
+Thank you.
+
+Best,
+Divyanshu
+"""
+
+
+
+
+def followup2(first_name):
+
+    return f"""
+Hi {first_name},
+
+Just one last follow-up from my side.
+
+If there aren't any internship openings at the moment, no worries. I'd be grateful if you could keep my resume in mind for future Data Analyst opportunities.
+
+Thank you for your time.
+
+Best,
+Divyanshu
+"""
